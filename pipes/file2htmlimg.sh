a0df953e96f34201038c6a98805d9ad1d6cfce81
ssarnobat <ss401533@gmail.com>
2016-09-02 21:25:14 -0700

    Minor changes (untested)

--- pipes/file2htmlimg.sh
+++ pipes/file2htmlimg.sh
@@ -2,7 +2,7 @@ PREFIX='http://netgear.rohidekar.com:44452'
 while read line
 do
   echo "$line" \
-	| perl -pe 's{(^.*jpg)\n}{  <img src="'$PREFIX'$1" height=100>\n}gi' \
+	| perl -pe 's{(^.*(jpg|gif))\n}{  <td><img src="'$PREFIX'$1" height=100></td>\n}gi' \
 	| perl -pe 's{(^/.*)\n}{$1\n}g' \
 	| perl -pe 's{^(/.*)}{</tr><tr>$1}g'
 #	| perl -pe 's{([^^])/}{$1</td><td>}g'
