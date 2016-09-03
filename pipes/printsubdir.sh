129bc72edb07a7dc7dc7779ae1f4a3cfe7cc07fb
ssarnobat <ss401533@gmail.com>
2016-09-02 22:20:02 -0700

    Minor changes (untested)

--- pipes/printsubdir.sh
+++ pipes/printsubdir.sh
@@ -1,6 +1,9 @@
 while read line
 do
-  echo "$line" | perl -pe 's{/}{</td><td>}g' \
-	| perl -pe 's{^</td>}{</tr><tr>}g'
+  ## Directories
+  echo "$line" | perl -pe 's{(.)/}{$1</td><td>}g' \
+	| perl -pe 's{\n}{</td><td>\n}g' \
+	| perl -pe 's{^/}{</td></tr><tr>/}g'
+  ## Images
   find "$line" -maxdepth 1 -type f -iname "*jpg" | head -5 | sh file2img.sh 
 done < "${1:-/dev/stdin}"
