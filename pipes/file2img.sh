0ef79647d2a1b25479e65f690bf5a311603ab7da
ssarnobat <ss401533@gmail.com>
2016-10-23 18:59:28 -0700

    Minor changes (untested)

--- pipes/file2img.sh
+++ pipes/file2img.sh
@@ -1,5 +1,5 @@
 PREFIX="http://netgear.rohidekar.com:44452"
 while read line
 do
-  echo "$line" | perl -pe 's{^(.*)\n}{<img src="'$PREFIX'$1" height=100>\n}g'
+  echo "$line" | perl -pe 's{^(.*)\n}{<img src="'$PREFIX'$1" height=33 onmouseenter="zoom(this)">\n}g'
 done < "${1:-/dev/stdin}"
--- pipes/printsubdir.sh
+++ pipes/printsubdir.sh
@@ -1,29 +1,31 @@
+#!/bin/bash
 # TODO : We need to make this recursive in the outer invocation but not the inner invocation
 
 echo "<table>"
 while read line
 do
-  ## cells for indenting current dir
-#  echo "$line" | perl -pe 's{(.)/}{$1</td><td>}g' \
-#	| perl -pe 's{\n}{</td><td>\n}g' \
-#	| perl -pe 's{^/}{</td></tr>\n<tr>/}g'
 
 	echo "<tr>"
 	echo "<td>"
-	basename $line
+	basename "$line"
 	echo "</td>"
 	echo "<td style='text-align : left'>"
   ## Images
-  find "$line" -maxdepth 1 -type f -iname "*jpg" | head -5 | sh file2img.sh 
+  find "$line" -maxdepth 1 -type f -iname "*jpg" \
+	| head -3 | sh file2img.sh 
 	echo "<td>"
 	echo "</tr>"
 		  
   ## Directories
   echo "<tr><td></td><td>"
-  find "$line" -mindepth 1 -maxdepth 1 -type d | sh printsubdir.sh #| perl -pe 's{(.*)}{<tr>$1</tr>}g'
+  find "$line" -mindepth 1 -maxdepth 1 -type d \
+        -not -regex '.*Preview.*' \
+        -not -regex '.*Thumbnail.*' \
+	-not -regex '.*ignore_iPhoto_Library.*"' \
+	| grep -v 'ignore_iPhoto' \
+	| sh printsubdir.sh
   echo "</td></tr>"
 
-#  	echo "</td></tr>"
 done < "${1:-/dev/stdin}"
 
 echo "</table>"
