a07387659a24f1e000ab37c18fe6d232452118e5
ssarnobat <ss401533@gmail.com>
2016-11-19 23:08:39 -0800

    Minor changes (untested)

--- pipes/img2buttonPanel.sh
+++ pipes/img2buttonPanel.sh
@@ -2,10 +2,7 @@
 IMG=$1
 
 FILE=`echo "$IMG" | perl -pe 's{<img.*?src=".*?44452([^"]*)".*>}{$1}g'`
-#FILE='/home/sarnobat/other/favorites/trash/Getting started · Bootstrap_files/carousel.jpg'
-#echo $FILE
 DIR=`dirname "$FILE"`
-#find "$DIR" -mindepth 1 -maxdepth 1 -type d | perl -MFile::Basename -pe 's{(.*)\n}{<button onclick=moveFile("'"$FILE"'","$1")>@{basename($1)}</button><br>\n}g'
 
 echo "<span>"
 echo $IMG
--- pipes/run.sh
+++ pipes/run.sh
@@ -5,6 +5,7 @@ ls -d /home/sarnobat/other/favorites/  | sh printsubdir.sh | tee -a favorites.ht
 
 cat head.html | tee other.html
 ls -d /home/sarnobat/other/  | sh printsubdir.sh | tee -a other.html
+cat head.html | tee other_sort.html ; cat other.html | grep '<img' | xargs -n 1 -d'\n' sh img2buttonPanel.sh  | tee -a other_sort.html
 
 cat head.html | tee atletico.html
 ls -d /media/sarnobat/e/new/Atletico  | sh printsubdir.sh | tee -a atletico.html
