8cdb8fbfe83cea81869a3f4488ae5e27da2ff603
ssarnobat <ss401533@gmail.com>
2016-11-19 20:30:02 -0800

    Minor changes (untested)

--- pipes/img2buttonPanel.sh
+++ pipes/img2buttonPanel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 IMG=$1
 echo $IMG
-FILE=`echo "$IMG" | perl -pe 's{<img.*?src=".*?44452([^"]*)".*>}{$1}g'`
-#FILE='/home/sarnobat/other/favorites/trash/Getting started · Bootstrap_files/carousel.jpg'
+#FILE=`echo "$IMG" | perl -pe 's{<img.*?src=".*?44452([^"]*)".*>}{$1}g'`
+FILE='/home/sarnobat/other/favorites/trash/Getting started · Bootstrap_files/carousel.jpg'
 echo $FILE
-DIR=`dirname "$FILE"
+DIR=`dirname "$FILE"`
 #FILE_SIMPLE=`basename "$FILE"`
-find "$DIR" -mindepth 1 -maxdepth 1 -type d | perl -pe 's{(.*)\n}{<button onclick="moveFile("'"$FILE"'","$1")">$1</button><br>\n}ges'
+find "$DIR" -mindepth 1 -maxdepth 1 -type d | perl -pe 's{(.*)\n}{<button onclick="moveFile("'"$FILE"'","$1")">$1</button><br>\n}g'
 echo
