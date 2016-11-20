cf01897eee7e1ce4b8874dae3e6cb964ff13a946
ssarnobat <ss401533@gmail.com>
2016-11-19 23:13:14 -0800

    Minor changes (untested)

--- pipes/file2buttonPanel.sh
+++ pipes/file2buttonPanel.sh
@@ -4,8 +4,8 @@ FILE=$1
 DIR=`dirname "$FILE"`
 
 echo "<span>"
-echo $FILE | xargs -n 1 -d'\n' sh file2img.sh
-find "$DIR" -mindepth 1 -maxdepth 1 -type d | perl -MFile::Basename -pe 's{^(.+)$}{"<button onclick='\''moveFile(\"'"$FILE"'\", \"" . basename($1)."\", this)'\''>" . basename($1) ."</button>"}ge'
+echo $FILE | sh file2img.sh
+#find "$DIR" -mindepth 1 -maxdepth 1 -type d | perl -MFile::Basename -pe 's{^(.+)$}{"<button onclick='\''moveFile(\"'"$FILE"'\", \"" . basename($1)."\", this)'\''>" . basename($1) ."</button>"}ge'
 echo "<br>"
 echo "</span>"
 echo
