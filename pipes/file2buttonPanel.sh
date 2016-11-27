c86e946f6de8191a7099717056741039b599a68b
ssarnobat <ss401533@gmail.com>
2016-11-26 16:20:49 -0800

    Minor changes (untested)

--- pipes/file2buttonPanel.sh
+++ pipes/file2buttonPanel.sh
@@ -5,7 +5,8 @@ DIR=`dirname "$FILE"`
 
 echo "<span>"
 echo $FILE | sh file2img.sh
-#find "$DIR" -mindepth 1 -maxdepth 1 -type d | perl -MFile::Basename -pe 's{^(.+)$}{"<button onclick='\''moveFile(\"'"$FILE"'\", \"" . basename($1)."\", this)'\''>" . basename($1) ."</button>"}ge'
+echo "<br>"
+find "$DIR" -mindepth 1 -maxdepth 1 -type d | perl -MFile::Basename -pe 's{^(.+)$}{"<button onclick='\''moveFile(\"'"$FILE"'\", \"" . basename($1)."\", this)'\''>" . basename($1) ."</button>"}ge'
 echo "<br>"
 echo "</span>"
 echo
--- pipes/run.sh
+++ pipes/run.sh
@@ -5,7 +5,8 @@ ls -d /home/sarnobat/other/favorites/  | sh printsubdir.sh | tee -a favorites.ht
 
 cat head.html | tee other.html
 ls -d /home/sarnobat/other/  | sh printsubdir.sh | tee -a other.html
-cat head.html | tee other_sort.html ; cat other.html | grep '<img' | xargs -n 1 -d'\n' sh img2buttonPanel.sh  | tee -a other_sort.html
+#cat head.html | tee other_sort.html ; cat other.html | grep '<img' | xargs -n 1 -d'\n' sh img2buttonPanel.sh  | tee -a other_sort.html
+cat head.html| tee other_sort.html; find '/e/Drive J/pictures/Other (new)/pictures/misc_sync_master/' -maxdepth 1 -iname "*jpg" | head  -800 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | tee -a other_sort.html
 
 cat head.html | tee atletico.html
 ls -d /media/sarnobat/e/new/Atletico  | sh printsubdir.sh | tee -a atletico.html
