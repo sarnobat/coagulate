6948f2462524a6500440e1d0e947c982ffc5eff2
ssarnobat <ss401533@gmail.com>
2017-03-15 00:02:05 -0700

    Cron daily autocommit

--- pipes/video2img.sh
+++ pipes/video2img.sh
@@ -1,10 +1,4 @@
-PREFIX="http://netgear.rohidekar.com:44452"
 while read line
 do
-  path="$line"
-  dir=`dirname "$path"`
-  file=`basename "$path"`
-echo $line
   echo "$line" | perl -MFile::Basename  -pe 's{^(.*)\n}{"<img src=\"http://netgear.rohidekar.com:44452" . dirname($1) . "/_thumbnails/" . basename($1) . ".jpg\" height=53 onmouseenter=\"zoom(this)\">\n"}ge' ;
-  echo "$file<br>sridhar"
 done < "${1:-/dev/stdin}"
