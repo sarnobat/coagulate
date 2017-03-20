bf9aba7dbcd60073231f777dc884c4010bc1c33f
ssarnobat <ss401533@gmail.com>
2017-03-20 00:01:14 -0700

    Cron daily autocommit

--- pipes/file2buttonPanel.sh
+++ pipes/file2buttonPanel.sh
@@ -6,7 +6,8 @@ DIR=`dirname "$FILE"`
 echo "<span>"
 echo $FILE | sh file2img.sh
 echo "<br>"
-find "$DIR" -mindepth 1 -maxdepth 1 -type d | perl -MFile::Basename -pe 's{^(.+)$}{"<button onclick='\''moveFile(\"'"$FILE"'\", \"" . basename($1)."\", this)'\'' value=\"" . basename($1) . "\">" . basename($1) ."</button>"}ge'
+find "$DIR/" -mindepth 1 -maxdepth 1 -type d -o -type l | sort | tee dirs.txt \
+	| perl -MFile::Basename -pe 's{^(.+)$}{"<button onclick='\''moveFile(\"'"$FILE"'\", \"" . basename($1)."\", this)'\'' value=\"" . basename($1) . "\">" . basename($1) ."</button>"}ge' | tee dirs_buttons.txt
 echo "<br>"
 echo "</span>"
 echo
--- pipes/run.sh
+++ pipes/run.sh
@@ -11,12 +11,16 @@ ls -d /home/sarnobat/other/  | sh printsubdir.sh | tee -a other.html >/dev/null
 #cat head.html | tee other_sort.html ; cat other.html | grep '<img' | xargs -n 1 -d'\n' sh img2buttonPanel.sh  | tee -a other_sort.html
 cd ~/github/coagulate/pipes; cat head.html| tee other_sort.html; find '/e/Drive J/pictures/Other (new)/pictures/misc_sync_master/' -maxdepth 1 -iname "*jpg" | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | tee -a other_sort.html >/dev/null
 
+cd ~/github/coagulate/pipes; cat head.html| tee images_sort.html; find '/Unsorted/new/images/' -maxdepth 1 -iname "*jpg" | shuf | grep -v '@' | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | tee -a images_sort.html >/dev/null
+
 cat head.html | tee atletico.html > /dev/null
 ls -d /media/sarnobat/e/new/Atletico  | sh printsubdir.sh |head -80 | tee -a atletico.html >/dev/null
 
 cat head.html | tee iPhone.html > /dev/null
 ls -d /media/sarnobat/e/new/Photos/iPhone  | sh printsubdir.sh | head -80 |tee -a iPhone.html  >/dev/null
 ls -d /media/sarnobat/Unsorted/new/Photos/iPhone | sh printsubdir.sh | head -80|tee -a iPhone.html >/dev/null
+
+ls -d /Unsorted/new/images/*jpg  | sh printsubdir.sh | head -80 | cat head.html - | tee images.html  >/dev/null
 
 # TODO: /home/sarnobat/sarnobat.git/index/cats.txt
 
