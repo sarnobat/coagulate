7165c3a110cec3df3662be0437176b2973b26a81
ssarnobat <ss401533@gmail.com>
2017-04-24 00:02:33 -0700

    Cron daily autocommit

--- pipes/run.sh
+++ pipes/run.sh
@@ -57,11 +57,11 @@ echo "Success: videos_atletico_sort.html"
 ## Sort (file2buttonPanel.sh)
 ##
 
-cd ~/github/coagulate/pipes; cat head.html| tee other_sort.html; find '/e/Drive J/pictures/Other (new)/pictures/misc_sync_master/' -maxdepth 1 -iname "*jpg" | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | tee other_sort.html >/dev/null
-cd ~/github/coagulate/pipes; cat head.html| tee images_sort.html; find '/Unsorted/new/images/' -maxdepth 1 -iname "*jpg" | shuf | grep -v '@' | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | tee images_sort.html >/dev/null
-cd ~/github/coagulate/pipes; find '/Unsorted/new/images/atletico' -maxdepth 1 -iname "*jpg" | shuf | grep -v '@' | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | cat head.html - | tee atletico_sort.html >/dev/null
-cd ~/github/coagulate/pipes; find '/Unsorted/new/images/atletico' -maxdepth 1 -type f -iname "*calderon*" -o -iname "*metropol*" | shuf | grep -v '@' | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | cat head.html - | tee atletico_stadium_sort.html >/dev/null
-
+cd ~/github/coagulate/pipes; find '/e/Drive J/pictures/Other (new)/pictures/misc_sync_master/' -maxdepth 1 -iname "*jpg" | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | cat head.html - | tee other_sort.html >/dev/null
+cd ~/github/coagulate/pipes; find '/Unsorted/new/images/' -maxdepth 1 -iname "*jpg" | shuf | grep -v '@' | head  -380 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | cat head.html - | tee images_sort.html >/dev/null
+cd ~/github/coagulate/pipes; find '/Unsorted/new/images/atletico' 	-maxdepth 1 -iname "*jpg" | shuf | grep -v '@' | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | cat head.html - | tee atletico_sort.html >/dev/null
+cd ~/github/coagulate/pipes; find '/Unsorted/new/images/atletico' 	-maxdepth 1 -type f -iname "*calderon*" -o -iname "*metropol*" | shuf | grep -v '@' | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | cat head.html - | tee atletico_stadium_sort.html >/dev/null
+cd ~/github/coagulate/pipes; find '/3TB/new/move_to_unsorted/images' 	-maxdepth 1 -type f | shuf | grep -v '@' | head  -80 |  xargs --delimiter '\n' --max-args=1 sh file2buttonPanel.sh  | perl -pe 's{height=33}{height=250}g' | cat head.html - | tee images_3tb_sort.html >/dev/null
 
 
 
