7600a654d02ed9ec4bac159d68130a0bff78218b
ssarnobat <ss401533@gmail.com>
2017-05-20 18:00:26 -0700

    Minor changes (untested)

--- server_files.groovy
+++ server_files.groovy
@@ -1,15 +1,10 @@
-import java.nio.charset.*;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLDecoder;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.WritableByteChannel;
@@ -22,10 +17,8 @@ import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
 import java.util.Date;
 import java.util.Hashtable;
-import java.util.Locale;
 import java.util.StringTokenizer;
 
-import javax.net.ssl.SSLContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.PathParam;
@@ -41,26 +34,6 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.http.ExceptionLogger;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.MethodNotSupportedException;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.InputStreamEntity;
-import org.apache.http.impl.nio.bootstrap.HttpServer;
-import org.apache.http.impl.nio.bootstrap.ServerBootstrap;
-import org.apache.http.impl.nio.reactor.IOReactorConfig;
-import org.apache.http.nio.entity.NStringEntity;
-import org.apache.http.nio.protocol.BasicAsyncRequestConsumer;
-import org.apache.http.nio.protocol.BasicAsyncResponseProducer;
-import org.apache.http.nio.protocol.HttpAsyncExchange;
-import org.apache.http.nio.protocol.HttpAsyncRequestConsumer;
-import org.apache.http.nio.protocol.HttpAsyncRequestHandler;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.ssl.SSLContexts;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -90,8 +63,6 @@ public class CoagulateFileServer {
 	    		@PathParam("filePath") String filePath1,
 	    		@HeaderParam("Range") String range) throws Exception {
 
-    Charset utf8Charset = Charset.forName("UTF-8");
-    Charset defaultCharset = Charset.defaultCharset();
 	        File audio;
 	        String filePath = filePath1;
 	        if (!filePath1.startsWith("/")) {
@@ -157,57 +128,46 @@ public class CoagulateFileServer {
 
 	private static class PartialContentServer {
 		static Response buildStream(final File asset, final String range, String contentType) throws Exception {
-// 			System.out.println("Coagulate.PartialContentServer.buildStream() 0");
 			
 			if (Paths.get("/home/sarnobat/trash/Coreografía.txt").toFile().exists()) {
-// 				System.out.println("Coagulate.PartialContentServer.buildStream() test file exists");
 			} else {
+				// I don't know why this doesn't work, but actual files with
+				// european chars coming from Chrome do work so we'll avoid printing
+				// this.
 				System.out.println("Coagulate.PartialContentServer.buildStream() test file doesn't exist");
 			}
-		if (!asset.exists()) {
-			System.out.println("Coagulate.PartialContentServer.buildStream() file doesn't exist: " + asset);
-	return Response.fail().status(404).build();
-}
-// 			System.out.println("Coagulate.PartialContentServer.buildStream() file exists: " + asset);
+			if (!asset.exists()) {
+				System.out.println("Coagulate.PartialContentServer.buildStream() file doesn't exist: " + asset);
+				return Response.status(404).build();
+			}
 
 	        if (range == null) {
-// 	        	System.out.println("Coagulate.PartialContentServer.buildStream() 1");
 	            StreamingOutput streamer = new StreamingOutput() {
 	                @Override
 	                public void write(OutputStream output) throws IOException, WebApplicationException {
-// System.out
-// 		.println("Coagulate.PartialContentServer.buildStream(...).new StreamingOutput() {...}.write()  asset = " + asset);
-	       //             @SuppressWarnings("resource")
-				FileChannel inputChannel;
-				try {
-					inputChannel = new FileInputStream(asset).getChannel();
-} catch (Exception e) {
-	System.out.println(e);
-	e.printStackTrace();
-	throw e;
-}
-// 			    System.out.println("output = " + output);
-	                    WritableByteChannel outputChannel = Channels.newChannel(output);
-	                    try {
-// 	                    	System.out.println("Coagulate.PartialContentServer.buildStream(...).new StreamingOutput() {...}.write() 2");
-	                        inputChannel.transferTo(0, inputChannel.size(), outputChannel);
-	                    } catch (Exception e) {
-				e.printStackTrace();
-				System.out.println(e);
-			    } finally {
+						FileChannel inputChannel;
+						try {
+							inputChannel = new FileInputStream(asset).getChannel();
+						} catch (Exception e) {
+							e.printStackTrace();
+							throw e;
+						}
+						WritableByteChannel outputChannel = Channels .newChannel(output);
+						try {
+							inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+		                } catch (Exception e) {
+							e.printStackTrace();
+					    } finally {
 	                        // closing the channels
 	                        inputChannel.close();
 	                        outputChannel.close();
-				System.out.println("Channels closed");
 	                    }
 	                }
 	            };
-	            System.out.println("Coagulate.PartialContentServer.buildStream() returning status 200");
 	            Response r =  Response.ok(streamer).status(200).header(HttpHeaders.CONTENT_LENGTH, asset.length()).header(HttpHeaders.CONTENT_TYPE, contentType).build();
-		    return r;
+	            return r;
 	        }
 
-	        System.out.println("Coagulate.PartialContentServer.buildStream() 2");
 	        String[] ranges = range.split("=")[1].split("-");
 	        int from = Integer.parseInt(ranges[0]);
 	        /**
@@ -257,7 +217,7 @@ public class CoagulateFileServer {
 		                length -= read;
 		            }
 		        } catch(java.io.IOException e) {
-		        	System.out.println("Broken pipe (we don't need to log this)");
+		        	//System.out.println("Broken pipe (we don't need to log this)");
 		        } finally {
 		            raf.close();
 		        }
@@ -268,161 +228,6 @@ public class CoagulateFileServer {
 		    }
 		}
 
-	}
-
-	@Deprecated
-	private static class NioFileServerWithStreamingVideoAndPartialContent {
-		static void startServer(int port) throws NoSuchAlgorithmException, KeyManagementException,
-				KeyStoreException, UnrecoverableKeyException, CertificateException, IOException,
-				InterruptedException {
-			SSLContext sslcontext = null;
-			if (port == 8443) {
-				// Initialize SSL context
-				URL url = NioFileServerWithStreamingVideoAndPartialContent.class.getResource("/my.keystore");
-				if (url == null) {
-					System.out.println("Keystore not found");
-					System.exit(1);
-				}
-				sslcontext = SSLContexts.custom()
-						.loadKeyMaterial(url, "secret".toCharArray(), "secret".toCharArray())
-						.build();
-			}
-
-			IOReactorConfig config = IOReactorConfig.custom().setSoTimeout(15000)
-					.setTcpNoDelay(true).build();
-
-			HttpServer server = ServerBootstrap.bootstrap()
-					.setListenerPort(port)
-					.setServerInfo("Test/1.1")
-					.setIOReactorConfig(config)
-					.setSslContext(sslcontext)
-					.setExceptionLogger(ExceptionLogger.STD_ERR)
-					.registerHandler("*", new HttpFileHandler()).create();
-
-			server.start();
-		}
-
-		private static class HttpFileHandler implements HttpAsyncRequestHandler<HttpRequest> {
-
-			@Override
-			public HttpAsyncRequestConsumer<HttpRequest> processRequest(final HttpRequest request,
-					final HttpContext context) {
-				// Buffer request content in memory for simplicity
-				return new BasicAsyncRequestConsumer();
-			}
-
-			@Override
-			public void handle(final HttpRequest request, final HttpAsyncExchange httpexchange,
-					final HttpContext context) throws HttpException, IOException {
-				HttpResponse response = httpexchange.getResponse();
-				handleInternal(request, response, context);
-				httpexchange.submitResponse(new BasicAsyncResponseProducer(response));
-			}
-
-			private static void handleInternal(HttpRequest request,
-					HttpResponse response, HttpContext context) throws HttpException,
-					IOException {
-
-				String method = request.getRequestLine().getMethod().toUpperCase(Locale.ENGLISH);
-				if (methodNotSupported(method)) {
-					throw new MethodNotSupportedException(method + " method not supported");
-				}
-
-				String target = request.getRequestLine().getUri().replaceAll(".width.*", "")
-						.replace("%20", " ");
-				File file = Paths.get(URLDecoder.decode(target, "UTF-8").replace("/_ 1", "/_+1"))
-						.toFile();
-				if (!file.canRead()) {
-					throw new RuntimeException("cannot read");
-				}
-				if (!file.exists()) {
-				
-					String error = "<html><body><h1>File <br>"
-							+ file.getPath() + "<br> not found</h1></body></html>";
-					response.setEntity(createEntity(error));
-					response.setStatusCode(HttpStatus.SC_NOT_FOUND);
-				
-				} else if (!file.canRead() || file.isDirectory()) {
-				
-					String error = "<html><body><h1>Access denied - for privacy</h1></body></html>";
-					response.setEntity(createEntity(error));
-					response.setStatusCode(HttpStatus.SC_FORBIDDEN);
-				} else {
-					response.setStatusCode(HttpStatus.SC_OK);
-					serveFileStreaming(response, file);
-				}
-			}
-
-			private static boolean methodNotSupported(String method) {
-				return !method.equals("GET") && !method.equals("HEAD") && !method.equals("POST");
-			}
-
-			private static NStringEntity createEntity(String error) {
-				return new NStringEntity(error, ContentType.create("text/html", "UTF-8"));
-			}
-
-			private static void serveFileStreaming(final HttpResponse response, File file) {
-				try {
-					final InputStream fis = new FileInputStream(file);
-					String mimeType = getMimeType(file);
-					HttpEntity body = new InputStreamEntity(fis, ContentType.create(mimeType));
-					response.setEntity(body);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
-
-			private static String getMimeType(File file) {
-				String mimeType;
-				Path path = Paths.get(file.getAbsolutePath());
-				if (file.getName().endsWith(".mp4")) {
-					String extension = FilenameUtils.getExtension(path.getFileName().toString());
-					mimeType = theMimeTypes.get(extension);
-				} else {
-					mimeType = "image/jpeg";
-				}
-				System.out.println("Coagulate.FileServerNio.HttpFileHandler.serveFileStreaming() mimetype = " + mimeType);
-				return mimeType;
-			}
-			
-			/**
-			 * Hashtable mapping (String)FILENAME_EXTENSION -> (String)MIME_TYPE
-			 */
-			private static Hashtable<String, String> theMimeTypes = new Hashtable<String, String>();
-	
-			static {
-				StringTokenizer st = new StringTokenizer(
-						"css		text/css " +
-								"htm		text/html " +
-								"html		text/html " +
-								"xml		text/xml " +
-								"txt		text/plain " +
-								"asc		text/plain " +
-								"gif		image/gif " +
-								"jpg		image/jpeg " +
-								"jpeg		image/jpeg " +
-								"png		image/png " +
-								"mp3		audio/mpeg " +
-								"mov		audio/quicktime " +
-								"m3u		audio/mpeg-url " +
-								"mp4		video/mp4 " +
-								"ogv		video/ogg " +
-								"webm		video/webm " +
-								"flv		video/x-flv " +
-								"mov		video/quicktime " +
-								"swf		application/x-shockwave-flash " +
-								"js			application/javascript " +
-								"pdf		application/pdf " +
-								"doc		application/msword " +
-								"ogg		application/x-ogg " +
-								"zip		application/octet-stream " +
-								"exe		application/octet-stream " +
-								"class		application/octet-stream ");
-				while (st.hasMoreTokens()) {
-					theMimeTypes.put(st.nextToken(), st.nextToken());
-				}
-			}
-		}
 	}
 
 	private static String fsPort ;
