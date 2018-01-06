f35af5135744b7298b4f100f4932d7aed291f232
ssarnobat <ss401533@gmail.com>
2018-01-06 15:12:07 -0800

    Minor changes (untested)

--- pipes/v1/file2htmlimg.sh
+++ pipes/v1/file2htmlimg.sh
@@ -1,9 +1,12 @@
+# This is for rapid development. Probably you'll need a java program as well for non-trivial transformations.
+
 PREFIX='http://netgear.rohidekar.com:44452'
 while read line
 do
   echo "$line" \
-	| perl -pe 's{(^.*(jpg|gif))\n}{  <td><img src="'$PREFIX'$1" height=100></td>\n}gi' \
-	| perl -pe 's{(^/.*)\n}{$1\n}g' \
-	| perl -pe 's{^(/.*)}{</tr><tr>$1}g'
+	| perl -pe 's{(^.*(jpg|gif))\n}{  <td><img src="'$PREFIX'$1" height=100 title="$1"></td>\n}gi' \
+	| perl -pe 's{(^.*(mwk))\n}{       <iframe src="'$PREFIX'$1" ></iframe>}g'
+#	| perl -pe 's{(^/.*)\n}{$1\n}g' \
+#	| perl -pe 's{^(/.*)}{</tr><tr>$1}g'
 #	| perl -pe 's{([^^])/}{$1</td><td>}g'
 done < "${1:-/dev/stdin}"
--- pipes/v1/run.sh
+++ pipes/v1/run.sh
@@ -1,1 +1,1 @@
-find /Unsorted/images/other/ -type d | head -60 | xargs -n 1 -d '\n' one_file | sh file2htmlimg.sh | tee index.html
+find /Unsorted/images/other/ -type d | head -60 | xargs -n 1 -d '\n' ~/github/coagulate/pipes/v1/one_file | sh ~/github/coagulate/pipes/v1/file2htmlimg.sh | tee index.html
