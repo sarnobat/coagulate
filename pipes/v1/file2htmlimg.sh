5043cb67ec7b3f6c5a20e15fdefa56af38e98ae1
ssarnobat <ss401533@gmail.com>
2018-01-06 19:30:09 -0800

    Minor changes (untested)

--- pipes/v1/file2htmlimg.sh
+++ pipes/v1/file2htmlimg.sh
@@ -4,8 +4,8 @@ PREFIX='http://netgear.rohidekar.com:44452'
 while read line
 do
   echo "$line" \
-	| perl -pe 's{(^.*(jpg|gif))\n}{  <td><img src="'$PREFIX'$1" height=100 title="$1"></td>\n}gi' \
-	| perl -pe 's{(^.*(mwk))\n}{       <iframe src="'$PREFIX'$1" ></iframe>}g'
+	| perl -pe 's{(^.*(jpg|gif))\n}{  <td><img src="'$PREFIX'$1" height=100 title="$1"></td>\n}gi'
+#	| perl -pe 's{(^.*(mwk))\n}{       <iframe src="'$PREFIX'$1" ></iframe>}g'
 #	| perl -pe 's{(^/.*)\n}{$1\n}g' \
 #	| perl -pe 's{^(/.*)}{</tr><tr>$1}g'
 #	| perl -pe 's{([^^])/}{$1</td><td>}g'
