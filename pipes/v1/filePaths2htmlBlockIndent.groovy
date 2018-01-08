e7b25732ae40021310a0879c81cd0c59636b3460
ssarnobat <ss401533@gmail.com>
2018-01-07 21:54:18 -0800

    Minor changes (untested)

--- pipes/v1/filePaths2htmlBlockIndent.groovy
+++ pipes/v1/filePaths2htmlBlockIndent.groovy
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -10,22 +11,34 @@ public class filePaths2htmlBlockIndent {
 		int lowestLevel = 0;
 		String close = "";
 		BufferedReader br = null;
+		Stack stack = new Stack();
 		try {
 			br = new BufferedReader(new InputStreamReader(System.in));
 			String line;
+			String initial;
 			while ((line = br.readLine()) != null) {
 				// log message
-				System.err.println("[DEBUG] current line is: " + line);
+				//System.err.println("[DEBUG] current line is: " + line);
+				
+				if (initial == null) {
+					initial = line;
+				}
+				
+				// If it's a directory...
 				if (line.startsWith("/") && !line.endsWith("mwk")) {
+					System.err.println("[DEBUG] Will be indented: " + line);
 					// is a directory
 					int count = StringUtils.countMatches(line, "/");
 					if (lowestLevel == 0) {
-						lowestLevel = count;
+						lowestLevel = count - 1;
 					}
-					String open = StringUtils.repeat("<blockquote>", count - lowestLevel);
-					close = StringUtils.repeat("</blockquote>", count - lowestLevel);
+					int level = count - lowestLevel;
+					String open = StringUtils.repeat("<blockquote>", level);
+					close = stack.isEmpty() ? "" : stack.pop();//StringUtils.repeat("</blockquote>", level);
+					String tobepushed = StringUtils.repeat("</blockquote>", level);
+					stack.push(tobepushed);
 					// program output
-					System.out.println(close +"\n"+ open + "\n" + line + "\n<br>\n");
+					System.out.println(close +"\n"+ open + "\n<h2>" + line.replace(initial,"") + "</h2>\n<br>\n");
 				} else {
 					System.out.println(line + "");
 				}
